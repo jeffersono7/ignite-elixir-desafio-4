b6a44d4400bd23b1bdffa3997ae7a5060af12f2a
Jefferson Farias <jefferson.farias7@gmail.com>
2021-12-24 11:42:59 -0300

    booking agent

---
+++ lib/bookings/agent.ex
@@ -0,0 +1,30 @@
+defmodule Flightex.Bookings.Agent do
+  use Agent
+
+  alias Flightex.Bookings.Booking
+
+  def start_link(_initial_state) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  @spec save(%Booking{}) :: {:ok, binary()}
+  def save(%Booking{id: id} = booking) do
+    Agent.update(__MODULE__, &save_booking(&1, id, booking))
+
+    {:ok, id}
+  end
+
+  @spec get(binary()) :: {:ok, %Booking{}} | {:error, binary()}
+  def get(id), do: Agent.get(__MODULE__, &get_booking(&1, id))
+
+  defp get_booking(state, id) do
+    case Map.get(state, id) do
+      nil -> {:error, "Booking not found"}
+      booking -> {:ok, booking}
+    end
+  end
+
+  defp save_booking(state, id, booking) do
+    Map.put(state, id, booking)
+  end
+end
